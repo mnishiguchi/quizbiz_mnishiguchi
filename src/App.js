584aa239e61cccadf23ff3b435175a3e52b6cffe
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2019-11-02 10:43:38 -0400

    Add components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Segment, Header, Container } from 'semantic-ui-react';
+import Amplify from 'aws-amplify';
+// import { withAuthenticator } from 'aws-amplify-react';
+
+import aws_exports from './aws-exports';
+
+import Quiz from './components/Quiz';
+import QuizPicker from './components/QuizPicker';
+
+Amplify.configure(aws_exports);
+Amplify.Logger.LOG_LEVEL = 'INFO';
 
 function App() {
+  const [activeQuiz, setActiveQuiz] = useState(null);
+
+  // componentDidMount
+  useEffect(() => {
+    document.title = 'QuizBiz';
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Segment className="inverted center aligned" style={{ minHeight: '200px' }}>
+        <Container>
+          <Header as="h1" textAlign="center" style={{ marginTop: '2em', color: 'white' }}>
+            Welcome to QuizBiz
+          </Header>
+        </Container>
+      </Segment>
+      <Container>
+        <QuizPicker activeQuiz={activeQuiz} propagateQuiz={setActiveQuiz} />
+        <Quiz activeQuiz={activeQuiz} />
+      </Container>
     </div>
   );
 }
 
+// export default withAuthenticator(App);
 export default App;
